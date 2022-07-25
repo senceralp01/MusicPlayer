5713dd7036415d18ef4b1164db08ec3a3eb8136e
sencer2022 <senceralpdogdu@gmail.com>
2022-07-26 02:06:17 +0300

    created constructors

---
+++ app.js
@@ -0,0 +1,16 @@
+const player = new MusicPlayer(musicList);
+
+let music = player.getMusic();
+console.log(music.getName());
+
+player.next();
+music = player.getMusic();
+console.log(music.getName());
+
+player.previous();
+music = player.getMusic();
+console.log(music.getName());
+
+player.next();
+music = player.getMusic();
+console.log(music.getName());
