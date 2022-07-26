9cb46bc4783c0fcf486707389c355aa3a61c18bc
sencer2022 <senceralpdogdu@gmail.com>
2022-07-26 04:58:30 +0300

    next&prev controls

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const next = document.querySelector("#controls #next");
 const player = new MusicPlayer(musicList);
 
 
+
 window.addEventListener("load", () => {
     let music = player.getMusic();
     displayMusic(music);
@@ -20,10 +21,56 @@ function displayMusic (music){
     title.innerText = music.getName();
     singer.innerText = music.singer;
     image.src = "img/" + music.img;
-    audio.src = "mp3/" + music.file;
+    audio.src = "mp3/" + music.file; // dikkat! ses dosyası
 
 }
 
 play.addEventListener("click", () => {
-    audio.play();
+    const isPlaying = container.classList.contains("playing");
+    isPlaying ? pauseMusic() : playMusic();
 })
+
+
+next.addEventListener("click", () => {
+    nextMusic();
+})
+
+
+prev.addEventListener("click", () => {
+    prevMusic();
+})
+
+function nextMusic(){
+    player.next();
+    let music = player.getMusic();
+    displayMusic(music);
+    playMusic();
+}
+
+function prevMusic(){
+    player.prev();
+    let music = player.getMusic();
+    displayMusic(music);
+    playMusic();
+}
+
+
+// function playMusic(){
+//     container.classList.add("playing");
+//     audio.play();
+// }
+const playMusic = () => {
+    container.classList.add("playing");
+    play.classList = "fa-solid fa-pause";
+    audio.play();
+}
+
+// function pauseMusic(){
+//     container.classList.remove("playing");
+//     audio.pause();
+// }
+const pauseMusic = () => {
+    container.classList.remove("playing");
+    play.classList = "fa-solid fa-play";
+    audio.pause();
+}
